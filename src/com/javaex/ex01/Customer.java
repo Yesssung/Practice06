fd8f443045b63ccefbc9b04dd388e4dacac0ff21
PARK YESUNG <cjsrnr0504@naver.com>
2024-04-19 16:51:03 +0900

    24.04.19 학원

--- src/com/javaex/ex01/Customer.java
+++ src/com/javaex/ex01/Customer.java
@@ -6,32 +6,98 @@ public class Customer extends Person{
 	private int point;
 	
 	public Customer() {
-		System.out.println("Customer 생성자 (0) 호출");
-	}
-	public Customer(String name, String hp, int cNo, int point) {
-		super(name,hp);
-		this.cNo = cNo;
-		this.point = point;
-		System.out.println("Customer 생성자 (4) 호출");
+		
 	}
 	
+	public Customer(String name, String hp, int cNo, int point) {
+		super(name, hp);
+		this.cNo = cNo;
+		this.point = point;
+	}
+
 	public int getcNo() {
 		return cNo;
 	}
+
 	public void setcNo(int cNo) {
 		this.cNo = cNo;
 	}
+
 	public int getPoint() {
 		return point;
 	}
+
 	public void setPoint(int point) {
 		this.point = point;
 	}
 	
-	@Override
 	public void showInfo() {
-		System.out.printf("#이름: %s #핸드폰: %s #고객번호: %d, #포인트번호: %d", super.getName(), super.getHp(), cNo/*this.cNo라고 써도 됌*/, point);
+		System.out.printf("%n#이름: %s, #핸드폰: %s, #고객번호: %d, #포인트점수: %d", super.getName(), super.getHp(), cNo, point);
 	}
 	
+
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//package com.javaex.ex01;
+//
+//public class Customer extends Person{
+//	
+//	private int cNo;
+//	private int point;
+//	
+//	public Customer() {
+//		System.out.println("Customer 생성자 (0) 호출");
+//	}
+//	public Customer(String name, String hp, int cNo, int point) {
+//		super(name,hp);
+//		this.cNo = cNo;
+//		this.point = point;
+//		System.out.println("Customer 생성자 (4) 호출");
+//	}
+//	
+//	public int getcNo() {
+//		return cNo;
+//	}
+//	public void setcNo(int cNo) {
+//		this.cNo = cNo;
+//	}
+//	public int getPoint() {
+//		return point;
+//	}
+//	public void setPoint(int point) {
+//		this.point = point;
+//	}
+//	
+//	@Override
+//	public void showInfo() {
+//		System.out.printf("#이름: %s #핸드폰: %s #고객번호: %d, #포인트번호: %d", super.getName(), super.getHp(), cNo/*this.cNo라고 써도 됌*/, point);
+//	}
+//	
+//	
+//}
--- src/com/javaex/ex01/Person.java
+++ src/com/javaex/ex01/Person.java
@@ -1,3 +1,4 @@
+
 package com.javaex.ex01;
 
 public class Person {
@@ -6,30 +7,98 @@ public class Person {
 	private String hp;
 	
 	public Person() {
-		System.out.println("생성자 Person(0) 호출");
-	}
-	public Person(String name, String hp) {
-		this.name = name;
-		this.hp = hp; ;
-		System.out.println("생성자 Person(2) 호출");
+		
 	}
 	
-	public String getName() {
-		return name; // return this.name 이라고 해도 됌!
+	public Person(String name, String hp) {
+		this.name = name;
+		this.hp = hp;
 	}
+
+	public String getName() {
+		return name;
+	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getHp() {
 		return hp;
 	}
+
 	public void setHp(String hp) {
 		this.hp = hp;
 	}
 	
 	public void showInfo() {
-		System.out.printf("#이름: %s #핸드폰: %s%n", name, hp);
+		System.out.printf("#이름: %s, #핸드폰: %s", name, hp);
 	}
 	
 	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//package com.javaex.ex01;
+//
+//public class Person {
+//	
+//	private String name;
+//	private String hp;
+//	
+//	public Person() { 
+//		System.out.println("생성자 Person(0) 호출");
+//	}
+//	public Person(String name, String hp) {
+//		this.name = name;
+//		this.hp = hp; ;
+//		System.out.println("생성자 Person(2) 호출");
+//	}
+//	
+//	public String getName() {
+//		return name; // return this.name 이라고 해도 됌!
+//	}
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//	public String getHp() {
+//		return hp;
+//	}
+//	public void setHp(String hp) {
+//		this.hp = hp;
+//	}
+//	
+//	public void showInfo() {
+//		System.out.printf("#이름: %s #핸드폰: %s%n", name, hp);
+//	}
+//	
+//	
+//}
